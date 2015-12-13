6710b48ef0adde7801acee339c53d65532aca1f8
flaprogrammer <flaprogrammer@gmail.com>
2015-12-13 21:16:09 +0300

    13 12

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-ymaps.ready(function () {
-	var myMap = new ymaps.Map('map', {
-		center: [59.830142, 30.544696],
-		zoom: 15,
-		// Обратите внимание, что в API 2.1 по умолчанию карта создается с элементами управления.
-		// Если вам не нужно их добавлять на карту, в ее параметрах передайте пустой массив в поле controls.
-		controls: []
+$(document).ready(function() {
+	ymaps.ready(function () {
+		var myMap = new ymaps.Map('map', {
+			center: [59.830142, 30.544696],
+			zoom: 15,
+			// Обратите внимание, что в API 2.1 по умолчанию карта создается с элементами управления.
+			// Если вам не нужно их добавлять на карту, в ее параметрах передайте пустой массив в поле controls.
+			controls: []
+		});
+
+		var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+			balloonContentBody: [
+				'<address>',
+				'<strong>Заводская, 15</strong>',
+				'<br/>',
+				'</address>'
+			].join(''),
+			hintContent: "Заводская, 15"
+		});
+
+		myMap.geoObjects.add(myPlacemark);
+
+
+
 	});
 
-	var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-		balloonContentBody: [
-			'<address>',
-			'<strong>Заводская, 15</strong>',
-			'<br/>',
-			'</address>'
-		].join(''),
-		hintContent: "Заводская, 15"
-	});
+	$('.flexslider').flexslider();
 
-	myMap.geoObjects.add(myPlacemark);
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $.attr(this, 'href') ).offset().top
+		}, 500);
+		return false;
+	});
 });
+
