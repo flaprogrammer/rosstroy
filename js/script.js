297d72b768de11e98b871e174729969e76985d28
lenar <lenar@smarthead.ru>
2016-01-18 19:16:31 +0300

    pres

--- js/script.js
+++ js/script.js
@@ -58,6 +58,21 @@ $(document).ready(function() {
             });
             $('.modal-wrapper').trigger('click');
     });
+	$(".presentation-modal-form").submit(function(e) { //устанавливаем событие отправки для формы с id=form
+			e.preventDefault();
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+	            type: "POST", //Метод отправки
+	            url: "send.php", //путь до php фаила отправителя
+	            data: form_data,
+	            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отправлено!");
+					window.location = "/docs/ZhK_Ekaterina_GOTOVO_final.pdf"
+               }
+            });
+            $('.modal-wrapper').trigger('click');
+    });
 	$(".form-flat").submit(function(e) { //устанавливаем событие отправки для формы с id=form
 			e.preventDefault();
             var form_data = $(this).serialize(); //собераем все данные из формы
